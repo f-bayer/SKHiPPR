Prettify autonomous ODE tests; truss seems to have wrong jacobian

skhippr/systems/autonomous.py
@@ -140,139 +140,153 @@ class Vanderpol(FirstOrderODE):
                 )
 
 
-def truss(
-    t: float | np.ndarray,
-    x: np.ndarray,
-    k: float,
-    c: float,
-    F: float,
-    a: float,
-    l_0: float,
-    m: float,
-) -> tuple[np.ndarray, dict[str, np.ndarray]]:
-    """
-    Dynamics  of a truss system.
-    The truss system is given by a block (mass 'm') that can move in horizontal direction under viscous damping ('c').
-    It is attached to a spring (unforced length 'l_0', spring constant 'k') whose other end is fixed at vertical height 'a'.
-    The block is forced by 'F' in horizontal direction.
-    This system can have up to three equilibria, depending on 'F'.
-    Parameters
-    ----------
-    t : float or np.ndarray
-        Current time or array of time values.
-    x : np.ndarray
-        State vector of the system, where x[0, ...] is position (q) and x[1, ...] is velocity (q_dot).
-    k : float
-        Spring constant.
-    c : float
-        Damping coefficient.
-    F : float
-        External force applied to the system.
-    a : float
-        Height of the spring.
-    l_0 : float
-        Rest length of the spring.
-    m : float
-        Mass of the system.
-    Returns
-    -------
-    f : np.ndarray
-        Time derivative of the state vector (i.e., [q_dot, q_ddot]).
-    derivatives : dict[str, np.ndarray]
-        Dictionary containing partial derivatives of the system dynamics with respect to parameters:
-        - "k": Derivative with respect to spring constant.
-        - "F": Derivative with respect to external force.
-        - "c": Derivative with respect to damping coefficient.
-        - "x": Jacobian of the system with respect to the state vector.
-    """
-    q = x[0, ...]
-    q_dot = x[1, ...]
+class Truss(FirstOrderODE):
 
-    derivatives = dict()
+    def __init__(
+        self,
+        x: np.ndarray,
+        k: float,
+        c: float,
+        F: float,
+        a: float,
+        l_0: float,
+        m: float,
+        **kwargs,
+    ):
+        super().__init__(autonomous=True, n_dof=2)
+        self.t = kwargs.get("t", None)
+        self.x = x
+        self.k = k
+        self.c = c
+        self.F = F
+        self.a = a
+        self.l_0 = l_0
+        self.m = m
 
-    f = np.zeros_like(x)
-    f[1, ...] = -k / m * q
-    f[1, ...] += k / m * q * l_0 / np.sqrt(a**2 + q**2)
-    derivatives["k"] = f / k
+    def parse_kwargs(self, **kwargs):
+        x = np.atleast_1d(kwargs.get("x", self.x))
+        k = kwargs.get("k", self.k)
+        c = kwargs.get("c", self.c)
+        F = kwargs.get("F", self.F)
+        a = kwargs.get("a", self.a)
+        l_0 = kwargs.get("l_0", self.l_0)
+        m = kwargs.get("m", self.m)
 
-    f[1, ...] += F / m - c / m * q_dot
-    derivatives["F"] = np.zeros_like(x)
-    derivatives["F"][1, ...] = 1 / m
-    derivatives["c"] = np.zeros_like(x)
-    derivatives["c"][1, ...] = x[1, ...] / m
+        if x.shape[0] != self.n_dof:
+            raise ValueError("first dimension of x must have length n_dof=2")
 
-    derivatives["x"] = np.zeros((f.shape[0], f.shape[0], *f.shape[1:]))
-    derivatives["x"][0, 1, ...] = 1
-    derivatives["x"][1, 1, ...] = -c / m
-    derivatives["x"][1, 0, ...] = -k / m
-    derivatives["x"][1, 0, ...] += k / m * l_0 / np.sqrt(a**2 + x[0, ...] ** 2)
-    derivatives["x"][1, 0, ...] -= k / m * l_0 * q**2 / (np.sqrt(a**2 + q**2) ** 3)
+        return x, k, c, F, a, l_0, m
 
-    derivatives["x"] = derivatives["x"]
-    derivatives["F"] = derivatives["F"]
-    derivatives["c"] = derivatives["c"]
-    derivatives["k"] = derivatives["k"]
+    def dynamics(self, **kwargs):
+        x, k, c, F, a, l_0, m = self.parse_kwargs(**kwargs)
+        q = x[0, ...]
+        q_dot = x[1, ...]
 
-    return f, derivatives
+        f = np.zeros_like(x)
+        f[1, ...] = -k / m * q
+        f[1, ...] += k / m * q * l_0 / np.sqrt(a**2 + q**2)
+        f[1, ...] += F / m - c / m * q_dot
+        return f
+
+    def df_dF(self, **kwargs):
+        x, _, _, _, _, _, m = self.parse_kwargs(**kwargs)
+        df_dF = np.zeros_like(x)
+        df_dF[1, ...] = 1 / m
+        return df_dF
+
+    def df_dc(self, **kwargs):
+        x, _, _, _, _, _, _, m = self.parse_kwargs(**kwargs)
+        df_dc = np.zeros_like(x)
+        df_dc[1, ...] = x[1, ...] / m
+        return df_dc
+
+    def df_dk(self, **kwargs):
+        x, _, _, _, a, l_0, m = self.parse_kwargs(**kwargs)
+        q = x[0, ...]
+        df_dk = np.zeros_like(x)
+        df_dk[1, ...] = -1 / m * q
+        df_dk[1, ...] += 1 / m * q * l_0 / np.sqrt(a**2 + q**2)
+        return df_dk
+
+    def df_dx(self, **kwargs):
+        x, k, c, _, a, l_0, m = self.parse_kwargs(**kwargs)
+        q = x[0, ...]
+
+        df_dx = np.zeros((x.shape[0], x.shape[0], *x.shape[1:]))
+        df_dx[0, 1, ...] = 1
+        df_dx[1, 1, ...] = -c / m
+        df_dx[1, 0, ...] = -k / m
+        df_dx[1, 0, ...] += k / m * l_0 / np.sqrt(a**2 + x[0, ...] ** 2)
+        df_dx[1, 0, ...] -= k / m * l_0 * q**2 / (np.sqrt(a**2 + q**2) ** 3)
+        return df_dx
 
 
-def blockonbelt(
-    t: float | np.ndarray,
-    x: np.ndarray,
-    epsilon: float,
-    k: float,
-    m: float,
-    Fs: float,
-    vdr: float,
-    delta: float,
-) -> tuple[np.ndarray, dict[str, np.ndarray]]:
-    """
-    Dynamics of a block-on-belt system with stick-slip motion and regularized Coulomb friction.
-    Parameters
-    ----------
-    t : float or np.ndarray
-        Current time or array of time points.
-    x : np.ndarray
-        State vector(s) of the system. The first row represents position(s), the second row velocity(ies).
-    epsilon : float
-        Regularization parameter for the friction law.
-    k : float
-        Spring constant.
-    m : float
-        Mass of the block.
-    Fs : float
-        Normal force.
-    vdr : float
-        Driving velocity of the belt.
-    delta : float
-        Slope parameter for the regularization of friction.
-    Returns
-    -------
-    f : np.ndarray
-        Time derivative(s) of the state vector(s).
-    derivatives : dict[str, np.ndarray]
-        Dictionary containing the Jacobian of the system with respect to the state vector, under the key "x".
-    """
+class BlockOnBelt(FirstOrderODE):
 
-    gamma_T = x[1, ...] - vdr
-    F_T = -Fs / (1 + delta * np.abs(gamma_T)) * 2 / np.pi * np.arctan(epsilon * gamma_T)
+    def __init__(
+        self,
+        x: np.ndarray,
+        epsilon: float,
+        k: float,
+        m: float,
+        Fs: float,
+        vdr: float,
+        delta: float,
+    ):
+        super().__init__(True, 2)
+        self.x = x
+        self.epsilon = epsilon
+        self.k = k
+        self.m = m
+        self.Fs = Fs
+        self.vdr = vdr
+        self.delta = delta
 
-    f = np.zeros_like(x)
-    f[0, ...] = x[1, ...]
-    f[1, ...] = -k / m * x[0, ...] + F_T / m
+    def parse_kwargs(self, **kwargs):
+        x = np.atleast_1d(kwargs.get("x", self.x))
+        epsilon = kwargs.get("epsilon", self.epsilon)
+        k = kwargs.get("k", self.k)
+        m = kwargs.get("m", self.m)
+        Fs = kwargs.get("Fs", self.Fs)
+        vdr = kwargs.get("vdr", self.vdr)
+        delta = kwargs.get("delta", self.delta)
 
-    df_dx = np.zeros((2, 2, *x.shape[1:]), dtype=x.dtype)
-    df_dx[0, 1, ...] = 1
-    df_dx[1, 0, ...] = -k / m
-    df_dx[1, 1, ...] = (Fs / m) * (
-        (np.arctan(epsilon * gamma_T) / (1 + delta * np.abs(gamma_T)) ** 2)
-        * (delta * np.sign(gamma_T) * 2 / np.pi)
-        - (1 / (1 + delta * np.abs(gamma_T)))
-        * (2 / np.pi)
-        / (1 + (epsilon * gamma_T) ** 2)
-        * epsilon
-    )
+        if x.shape[0] != self.n_dof:
+            raise ValueError("first dimension of x must have length n_dof=2")
+        return x, epsilon, k, m, Fs, vdr, delta
 
-    derivatives = dict()
-    derivatives["x"] = df_dx
-    return f.squeeze(), derivatives
+    def dynamics(self, **kwargs):
+        x, epsilon, k, m, Fs, vdr, delta = self.parse_kwargs(**kwargs)
+
+        gamma_T = x[1, ...] - vdr
+        F_T = (
+            -Fs
+            / (1 + delta * np.abs(gamma_T))
+            * 2
+            / np.pi
+            * np.arctan(epsilon * gamma_T)
+        )
+
+        f = np.zeros_like(x)
+        f[0, ...] = x[1, ...]
+        f[1, ...] = -k / m * x[0, ...] + F_T / m
+        return f
+
+    def derivative(self, variable, **kwargs):
+        if variable == "x":
+            x, epsilon, k, m, Fs, vdr, delta = self.parse_kwargs(**kwargs)
+            gamma_T = x[1, ...] - vdr
+            df_dx = np.zeros((2, 2, *x.shape[1:]), dtype=x.dtype)
+            df_dx[0, 1, ...] = 1
+            df_dx[1, 0, ...] = -k / m
+            df_dx[1, 1, ...] = (Fs / m) * (
+                (np.arctan(epsilon * gamma_T) / (1 + delta * np.abs(gamma_T)) ** 2)
+                * (delta * np.sign(gamma_T) * 2 / np.pi)
+                - (1 / (1 + delta * np.abs(gamma_T)))
+                * (2 / np.pi)
+                / (1 + (epsilon * gamma_T) ** 2)
+                * epsilon
+            )
+            return df_dx
+        else:
+            return super().derivative(variable, **kwargs)

test/test_vanderpol.py
@@ -1,87 +1,103 @@
 import numpy as np
 import pytest
-from skhippr.systems.autonomous import Vanderpol
+from skhippr.systems.autonomous import Vanderpol, Truss, BlockOnBelt
 from skhippr.math import finite_differences
 
 
-@pytest.mark.parametrize("n_samples", [1, 100])
-def test_vanderpol_inputs(n_samples):
-    """
-    Test the vanderpol function when t is an array and x is a 2xL array.
-    """
+@pytest.fixture(scope="module", params=["vanderpol", "truss", "blockonbelt"])
+def ode_setting(x, request):
 
+    match request.param:
+        case "vanderpol":
+            params = {"nu": 1.5}
+            return params, Vanderpol(x=x, **params)
+        case "truss":
+            params = {"k": 100.0, "c": 0.5, "F": 1.0, "a": 0.1, "l_0": 1.0, "m": 1.0}
+            return params, Truss(x=x, **params)
+        case "blockonbelt":
+            params = {"epsilon": 0.1, "k": 1, "m": 1, "Fs": 0.1, "vdr": 2, "delta": 0.5}
+            return params, BlockOnBelt(x=x, **params)
+
+
+@pytest.fixture(scope="module", params=[1, 100])
+def x(request):
+    n_samples = request.param
     if n_samples == 1:
-        t = 1.0
         x = np.random.rand(2)
-        df_dx_shape_expected = (2, 2)
     else:
-        t = np.linspace(1, 10, n_samples)
-        x = np.random.rand(2, len(t))
-        df_dx_shape_expected = (2, 2, n_samples)
+        x = np.random.rand(2, n_samples)
 
-    nu = 1.5
+    return x
 
-    vdp = Vanderpol(t=t, x=x, nu=nu)
-    f = vdp.dynamics()
 
+@pytest.mark.parametrize("n_samples", [1, 100])
+def test_f(ode_setting, n_samples):
+    """
+    Test the ODE function when x is either a 1-D or 2-D array.
+    """
+    _, ode = ode_setting
+    x = ode.x
+    df_dx_shape_expected = (2, *x.shape)
+
+    f = ode.dynamics()
     assert f.shape == x.shape, f"Expected f to have shape {x.shape}, but got {f.shape}"
 
-    df_dx = vdp.derivative("x")
 
+def test_mismatched_ndof(ode_setting):
+    """
+    Test the duffing function with mismatched n_dof.
+    """
+
+    params, ode = ode_setting
+    x = np.random.rand(3, len(t))  # 3xL array (incorrect dimension)
+    ode.x = x  # no error detected yet
+
+    with pytest.raises(ValueError):
+        f = ode.dynamics()
+
+    with pytest.raises(ValueError):
+        df_dx = ode.derivative("x")
+        pass
+
+    with pytest.raises(ValueError):
+        df_dx = ode.derivative("nu")
+        pass
+
+
+def test_derivatives(ode_setting):
+    """Verify that all derivatives match finite difference derivative"""
+    params, ode = ode_setting
+    x = ode.x
+
+    # Check derivative w.r.t. x
+    df_dx_shape_expected = (2, *x.shape)
+    df_dx = ode.derivative("x")
     assert (
         df_dx.shape == df_dx_shape_expected
     ), f"Expected df_dx to have shape {df_dx_shape_expected}, but got {df_dx.shape}"
 
-    df_dnu = vdp.derivative("nu")
-    assert (
-        df_dnu.shape == x.shape
-    ), f"Expected df_dmu to have shape {x.shape}, but got {df_dnu.shape}"
-
-
-def test_vdp_mismatched_ndof():
-    """
-    Test the duffing function with mismatched n_dof.
-    """
-    t = np.linspace(0, 10, 100)  # Array of time points
-    x = np.random.rand(3, len(t))  # 3xL array (incorrect dimension)
-    nu = 1.5
-
-    vdp = Vanderpol(t=t, x=x, nu=nu)  # no error detected yet
-
-    with pytest.raises(ValueError):
-        f = vdp.dynamics()
-
-    with pytest.raises(ValueError):
-        df_dx = vdp.derivative("x")
-        pass
-
-    with pytest.raises(ValueError):
-        df_dx = vdp.derivative("nu")
-        pass
-
-
-@pytest.mark.parametrize("variable", ["x", "nu", "t"])
-def test_duffing_derivatives(params_duffing, variable):
-    """Verify that the returned derivative matches finite difference derivative"""
-    t = np.linspace(0, 10, 100)  # Array of time points
-    x = np.random.rand(2, len(t))  # 2xL array
-    nu = 1.5
-
-    vdp = Vanderpol(x=x, nu=nu, t=t)
-
-    kwargs = {"nu": nu, "x": x, "t": t}
-
-    if variable not in ("x", "nu"):
-        with pytest.raises(AttributeError):
-            deriv_vdp = vdp.derivative(variable, **kwargs)
-        return
-
-    deriv_vdp = vdp.derivative(variable, **kwargs)
-    deriv_finite_diff = finite_differences(vdp.dynamics, kwargs, variable, 1e-5)
-
+    params["x"] = x
+    df_dx_fd = finite_differences(ode.dynamics, params, "x", 1e-5)
     assert np.allclose(
-        deriv_finite_diff, deriv_vdp, 1e-3, 1e-3
-    ), f"{variable} derivative does not match FD derivative with error {np.linalg.norm(deriv_finite_diff - deriv_vdp)}"
+        df_dx_fd, df_dx, 1e-3, 1e-3
+    ), f"df_dx does not match FD derivative with max error {np.max(np.abs(df_dx_fd - df_dx))}"
+
+    del params["x"]
+    # Check derivatives w.r.t scalar parameters
+    for variable in params:
+        df_dvar = ode.derivative(variable)
+        assert (
+            df_dvar.shape == x.shape
+        ), f"Expected df_dmu to have shape {x.shape}, but got {df_dvar.shape}"
+
+        df_dvar_fd = finite_differences(ode.dynamics, params, variable, 1e-5)
+        assert np.allclose(
+            df_dvar_fd, df_dvar, 1e-3, 1e-3
+        ), f"{variable} derivative does not match FD derivative with max error {np.max(np.abs(df_dvar_fd - df_dvar))}"
+
+    # Check that derivative w.r.t a non-parameter fails
+    with pytest.raises(AttributeError):
+        df_dvar = ode.derivative("no_param")
 
 
 if __name__ == "__main__":