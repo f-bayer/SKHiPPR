Fix finite differences derivative

skhippr/systems/AbstractSystems.py
@@ -8,10 +8,10 @@ from skhippr.math import finite_differences
 
 
 class FirstOrderODE(ABC):
-    def __init__(self, autonomous, n_dof, **kwargs):
+    def __init__(self, autonomous: bool, n_dof: int, **kwargs):
         """The constructor must set the number of degrees of freedom as well as all required parameter values (including the initial state) as properties."""
-        self.autonomous: bool
-        self.n_dof: int
+        self.autonomous = autonomous
+        self.n_dof = n_dof
 
     @abstractmethod
     def dynamics(self, **kwargs) -> np.ndarray:
@@ -25,6 +25,9 @@ class FirstOrderODE(ABC):
 
     def derivative(self, variable, h_fd=1e-4, **kwargs):
         """Return the partial derivative of f w.r.t <variable> as a self-n_dof x self.n_dof numpy array. Recommended implementation using switch/case and individual methods for all required derivatives."""
+        if variable not in kwargs:
+            kwargs[variable] = getattr(self, variable)
+
         derivative = finite_differences(
             self.dynamics, kwargs=kwargs, variable=variable, h_step=h_fd
         )

test/test_duffing.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
-from skhippr.systems.nonautonomous import duffing
+
+from skhippr.systems.nonautonomous import Duffing
 
 
 def test_duffing_array_inputs(params_duffing):
@@ -10,21 +11,24 @@ def test_duffing_array_inputs(params_duffing):
     t = np.linspace(0, 10, 100)  # Array of time points
     x = np.random.rand(2, len(t))  # 2xL array
 
-    f, derivatives = duffing(t, x, **params_duffing[1])
+    duff = Duffing(t=t, x=x, **params_duffing[1])
+
+    f = duff.dynamics()
 
     # Check dimensions of the output
     assert f.shape == x.shape, f"Expected f to have shape {x.shape}, but got {f.shape}"
-    assert derivatives["x"].shape == (
+
+    df_dx = duff.derivative("x")
+    assert df_dx.shape == (
         2,
         2,
         len(t),
     ), f"Expected df_dx to have shape {(2, 2, len(t))}, but got {f.shape}"
-    for key in derivatives:
-        if key == "x":
-            continue
+    for key in ["F", "omega", "alpha"]:
+        df_dkey = duff.derivative(key)
         assert (
-            derivatives[key].shape == x.shape
-        ), f"Expected df_d{key} to have shape {x.shape}, but got {derivatives.shape}"
+            df_dkey.shape == x.shape
+        ), f"Expected df_d{key} to have shape {x.shape}, but got {df_dkey.shape}"
 
 
 def test_duffing_scalar_inputs(params_duffing):
@@ -34,20 +38,22 @@ def test_duffing_scalar_inputs(params_duffing):
     t = 1.0  # Scalar time point
     x = np.random.rand(2)  # 1-dimensional array of length 2
 
-    # Call the duffing function
-    f, derivatives = duffing(t, x, **params_duffing[1])
+    duff = Duffing(t=t, x=x, **params_duffing[1])
+    f = duff.dynamics()
 
     assert f.shape == x.shape, f"Expected f to have shape {x.shape}, but got {f.shape}"
-    assert derivatives["x"].shape == (
+
+    df_dx = duff.derivative("x")
+    assert df_dx.shape == (
         2,
         2,
     ), f"Expected df_dx to have shape {(2, 2)}, but got {f.shape}"
-    for key in derivatives:
-        if key == "x":
-            continue
+
+    for key in ["F", "omega", "alpha"]:
+        df_dkey = duff.derivative(key)
         assert (
-            derivatives[key].shape == x.shape
-        ), f"Expected df_d{key} to have shape {x.shape}, but got {derivatives.shape}"
+            df_dkey.shape == x.shape
+        ), f"Expected df_d{key} to have shape {x.shape}, but got {df_dkey.shape}"
 
 
 def test_duffing_mismatched_ndof(params_duffing):