480c324ec5b807e010923e862a0691d253f07363
Fabia Bayer <bayer@inm.uni-stuttgart.de>
2025-07-23 10:29:52 +0200

    Fix EquationSystem copying tests

--- test/test_branchpoint_copy.py
+++ test/test_branchpoint_copy.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 from copy import copy
-from skhippr.problems.newton import NewtonSolver
+from skhippr.problems.newton import EquationSystem
 from skhippr.problems.continuation import BranchPoint
 from skhippr.systems.AbstractSystems import AbstractEquation
 
@@ -17,11 +17,13 @@ class CircleExplicit(AbstractEquation):
         self.other = other
 
     def residual_function(self):
-        return np.array(
-            [
-                self.x[0] ** 2 + self.x[1] ** 2 - self.radius**2,
-                self.x[1] * np.cos(self.theta) - self.x[0] * np.sin(self.theta),
-            ]
+        return np.squeeze(
+            np.array(
+                [
+                    self.x[0] ** 2 + self.x[1] ** 2 - self.radius**2,
+                    self.x[1] * np.cos(self.theta) - self.x[0] * np.sin(self.theta),
+                ]
+            )
         )
 
     def closed_form_derivative(self, variable):
@@ -30,19 +32,22 @@ class CircleExplicit(AbstractEquation):
                 return np.array(
                     [
                         [2 * self.x[0], 2 * self.x[1]],
-                        [-np.sin(self.theta), np.cos(self.theta)],
+                        [
+                            -np.sin(np.squeeze(self.theta)),
+                            np.cos(np.squeeze(self.theta)),
+                        ],
                     ]
                 )
             case "radius":
-                return np.array([[-2 * self.radius], [0]])
+                radius = np.atleast_1d(self.radius)
+                return np.array([-2 * radius, [0]])
             case "theta":
+                theta = np.atleast_1d(self.theta)
                 return np.array(
                     [
                         [0],
-                        [
-                            -self.x[1] * np.sin(self.theta)
-                            - self.x[0] * np.cos(self.theta)
-                        ],
+                        -self.x[1] * np.sin(self.theta)
+                        - self.x[0] * np.cos(self.theta),
                     ]
                 )
             case "other":
@@ -52,7 +57,6 @@ class CircleExplicit(AbstractEquation):
             case _:
                 raise NotImplementedError
 
-@pytest.
 
 @pytest.fixture
 def circle(solver):
@@ -61,175 +65,124 @@ def circle(solver):
     circ = CircleExplicit(
         x=np.array([1.1, 0.1]), radius=radius, theta=theta, other=(100, 10, 0)
     )
-    problem = NewtonSolver(
-        residual_function=circle_explicit,
-        initial_guess=np.array(),
-        verbose=True,
-        radius=radius,
-        theta=theta,
-        other=(100, 10, 0),
-    )
-    problem.solve()
-    assert problem.converged
-    return problem
+    circle_sys = EquationSystem([circ], ["radius", "theta"], None)
+
+    solver.solve(circle_sys)
+    assert circle_sys.solved
+    return circle_sys
 
 
-def compare_attributes(sol1, sol2, attr, is_list=False):
+def compare_attributes(sys1, sys2, attr, mutable=False):
     """Tests whether the sol1 and sol2 and its attributes are same (reference) and/or equal (value).
     Results are expected do differ for different ways of constructing sol1 and sol2 (see the tests).
     sol1, sol2 may be NewtonSolutions or BranchPoints."""
 
-    same = sol1 is sol2
-    same_attr = getattr(sol1, attr) is getattr(sol2, attr)
-    equal_attr = np.all(getattr(sol1, attr) == getattr(sol2, attr))
+    same = sys1 is sys2
+    same_attr = getattr(sys1, attr) is getattr(sys2, attr)
+    equal_attr = np.all(getattr(sys1, attr) == getattr(sys2, attr))
 
-    if is_list:
-        getattr(sol1, attr)[0] = getattr(sol1, attr)[0] + 2
-        attr_changed = getattr(sol1, attr)[0] == getattr(sol2, attr)[0]
+    if mutable:
+        getattr(sys1, attr)[0] = getattr(sys1, attr)[0] + 2
+        attr_changed = getattr(sys1, attr)[0] == getattr(sys2, attr)[0]
+        attr_eq_changed = (
+            getattr(sys1.equations[0], attr)[0] == getattr(sys2.equations[0], attr)[0]
+        )
     else:
-        setattr(sol1, attr, getattr(sol1, attr) + 2)
-        attr_changed = getattr(sol1, attr) == getattr(sol2, attr)
+        setattr(sys1, attr, getattr(sys1, attr) + 2)
+        attr_changed = getattr(sys1, attr) == getattr(sys2, attr)
+        attr_eq_changed = getattr(sys1.equations[0], attr) == getattr(
+            sys2.equations[0], attr
+        )
 
-    same_attr_after_change = getattr(sol1, attr) is getattr(sol2, attr)
-
-    return same, same_attr, equal_attr, attr_changed, same_attr_after_change
+    return same, same_attr, equal_attr, attr_changed, attr_eq_changed
 
 
-def test_solution_reference(prb):
+def test_no_copy(circle):
     # Directly duplicate the reference - everything should always be the same
-    sol2 = prb
-    same, same_attr, equal_attr, attr_changed, same_attr_after_change = (
-        compare_attributes(prb, sol2, "theta")
+    circle2 = circle
+    same, same_attr, equal_attr, attr_changed, attr_eq_changed = compare_attributes(
+        circle, circle2, "theta", mutable=False
     )
     assert same
     assert same_attr
     assert equal_attr
     assert attr_changed
-    assert same_attr_after_change
+    assert attr_eq_changed
 
 
-def test_solution_shallow_copy_theta(prb):
-    # Shallow-copy the reference.
+def test_copy(circle):
+    # Shallow-copy the reference using copy.copy() (not recommended)
     # Changes to the (immutable) attribute directly overwrite.
-    sol2 = copy(prb)
-    same, same_attr, equal_attr, attr_changed, same_attr_after_change = (
-        compare_attributes(prb, sol2, "theta", is_list=False)
+    circle2 = copy(circle)
+    same, same_attr, equal_attr, attr_changed, attr_eq_changed = compare_attributes(
+        circle, circle2, "theta", mutable=False
+    )
+    assert not same
+    assert same_attr
+    assert equal_attr
+    assert not attr_changed  # because changes in the equations do not reflect "upwards"
+    assert attr_eq_changed
+
+
+def test_duplicate(circle):
+    # Duplicate the reference properly using its method, shallow-copying the equations.
+    circle2 = circle.duplicate()
+    same, same_attr, equal_attr, attr_changed, attr_eq_changed = compare_attributes(
+        circle, circle2, "theta", mutable=False
     )
     assert not same
     assert same_attr
     assert equal_attr
     assert not attr_changed
-    assert not same_attr_after_change
+    assert not attr_eq_changed
 
 
-def test_solution_shallow_copy_x(prb):
-    # Shallow-copy the reference.
-    # Changes within mutable attributes persist.
-    sol2 = copy(prb)
-    same, same_attr, equal_attr, attr_changed, same_attr_after_change = (
-        compare_attributes(prb, sol2, "x", is_list=True)
+def test_duplicate_mutable(circle):
+    # Duplicating retains references of the equations to the same mutable objects
+    circle = EquationSystem(circle.equations, ["x"], None)
+    circle2 = circle.duplicate()
+    same, same_attr, equal_attr, attr_changed, attr_eq_changed = compare_attributes(
+        circle, circle2, "x", mutable=True
     )
     assert not same
     assert same_attr
     assert equal_attr
     assert attr_changed
-    assert same_attr_after_change
+    assert attr_eq_changed
 
 
-def test_solution_shallow_copy_x_solve(prb):
+@pytest.mark.parametrize("mutable", [False, True])
+def test_solve(solver, circle, mutable):
     # Shallow-copy the reference.
     # The attribute x is overwritten during solve().
 
-    prb.theta += 0.5
-    prb.reset()
-    sol2 = copy(prb)
+    if mutable:
+        circle = EquationSystem(circle.equations, ["x"], None)
+        solver.solve(circle)
 
-    prb.solve()
-    sol2.solve()
+    assert circle.solved
+    try:
+        circle.theta += 0.5
+        assert not circle.solved
+    except AttributeError:
+        # theta is not part of the unknowns
+        circle.equations[0].theta += 0.5
+        circle.solved = False
+
+    circle2 = circle.duplicate()
+    solver.solve(circle)
+
+    assert not circle2.solved
+    solver.solve(circle2)
+    assert circle2.solved
+
+    if mutable:
+        attr = "x"
+    else:
+        attr = "theta"
 
     same, same_attr, equal_attr, attr_changed, same_attr_after_change = (
-        compare_attributes(prb, sol2, "x", is_list=True)
-    )
-    assert not same
-    assert not same_attr
-    assert equal_attr
-    assert not attr_changed
-    assert not same_attr_after_change
-
-
-def test_branchpoint_reference(prb):
-    # Directly duplicate the branch point - everything should always be the same
-    bp = BranchPoint(problem=prb, key_param="theta", value_param=prb.theta)
-    bp2 = bp
-    same, same_attr, equal_attr, attr_changed, same_attr_after_change = (
-        compare_attributes(bp, bp2, "theta", is_list=False)
-    )
-    assert same
-    assert same_attr
-    assert equal_attr
-    assert attr_changed
-    assert same_attr_after_change
-
-
-def test_branchpoint_wrong_copy_theta(prb):
-    # (Wrongly) generating a new branch point with reference to the same _problem
-    # As attributes are deferred to problem, all changes are reflected in branch point.
-    bp = BranchPoint(problem=prb, key_param="theta", value_param=prb.theta)
-    bp2 = BranchPoint(problem=bp._problem, key_param="theta", value_param=prb.theta)
-    same, same_attr, equal_attr, attr_changed, same_attr_after_change = (
-        compare_attributes(bp, bp2, "theta", is_list=False)
-    )
-    assert not same
-    assert same_attr
-    assert equal_attr
-    assert attr_changed
-    assert same_attr_after_change
-
-
-def test_branchpoint_shallow_copy_radius(prb):
-    # Correctly generating a new branch point with reference to the copied _problem
-    # Then, both BranchPoints behave like their _problems w.r.t non-continuation parameters.
-    bp = BranchPoint(problem=prb, key_param="theta", value_param=prb.theta)
-    bp2 = BranchPoint(
-        problem=bp.copy_problem(), key_param="theta", value_param=prb.theta
-    )
-    same, same_attr, equal_attr, attr_changed, same_attr_after_change = (
-        compare_attributes(bp, bp2, "radius", is_list=False)
-    )
-    assert not same
-    assert same_attr
-    assert equal_attr
-    assert not attr_changed
-    assert not same_attr_after_change
-
-
-def test_branchpoint_shallow_copy_other(prb):
-    # Correctly generating a new branch point with reference to the copied _problem
-    # Then, both BranchPoints behave like their _problems w.r.t non-continuation parameters.
-    prb.other = [1, 2, 3]
-    bp = BranchPoint(problem=prb, key_param="theta", value_param=prb.theta)
-    bp2 = BranchPoint(
-        problem=bp.copy_problem(), key_param="theta", value_param=prb.theta
-    )
-    same, same_attr, equal_attr, attr_changed, same_attr_after_change = (
-        compare_attributes(bp, bp2, "other", is_list=True)
-    )
-    assert not same
-    assert same_attr
-    assert equal_attr
-    assert attr_changed
-    assert same_attr_after_change
-
-
-def test_branchpoint_shallow_copy_theta(prb):
-    # Correctly generating a new branch point with reference to the copied _problem
-    # x and parameter are overwritten (i.e., link is destroyed) during creation of BranchPoint (in NewtonSolution.__init__())
-    bp = BranchPoint(problem=prb, key_param="theta", value_param=prb.theta)
-    bp2 = BranchPoint(
-        problem=bp.copy_problem(), key_param="theta", value_param=prb.theta
-    )
-    same, same_attr, equal_attr, attr_changed, same_attr_after_change = (
-        compare_attributes(bp, bp2, "theta", is_list=False)
+        compare_attributes(circle, circle2, attr, mutable=mutable)
     )
     assert not same
     assert not same_attr
