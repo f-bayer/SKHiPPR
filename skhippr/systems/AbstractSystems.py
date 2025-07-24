131dc576d5511ec1bef283e58b8b923a27c9df0f
Fabia Bayer <bayer@inm.uni-stuttgart.de>
2025-07-24 08:56:55 +0200

    Write a new AbstractCycleEquation class that  encodes a periodic solution of the ODE

--- skhippr/systems/AbstractSystems.py
+++ skhippr/systems/AbstractSystems.py
@@ -231,3 +231,61 @@ class FirstOrderODE(AbstractEquation):
     def closed_form_derivative(self, variable, t=None, x=None):
         # Provide an interface which offers t and x
         return super().closed_form_derivative(variable)
+
+
+class AbstractCycleEquation(ABC, AbstractEquation):
+    def __init__(
+        self, ode: FirstOrderODE, omega=None, period_k=1, stability_method=None
+    ):
+        super().__init__(stability_method)
+        self.ode = ode
+
+        if omega is not None:
+            ode.omega = omega
+
+        self.period_k = period_k
+
+    @property
+    def T(self):
+        return 2 * np.pi / self.omega
+
+    @T.setter
+    def T(self, value):
+        self.omega = 2 * np.pi / value
+
+    @property
+    def T_solution(self):
+        return self.T * self.period_k
+
+    @property
+    def omega_solution(self):
+        "Returns the angular frequency of the periodic solution (factor_k times the excitation frequency)."
+        return self.omega / self.period_k
+
+    @property
+    def T(self):
+        return 2 * np.pi / self.omega
+
+    @T.setter
+    def T(self, value):
+        self.omega = 2 * np.pi / value
+
+    def __getattr__(self, name):
+        """Custom attribute extension that searches for attributes also in the ode"""
+        if "ode" in self.__dict__:
+            return getattr(self.ode, name)
+        else:
+            raise AttributeError(
+                f"'{str(self.__class__)}' object has no attribute '{name}'"
+            )
+
+    def __setattr__(self, name, value) -> None:
+        """Custom attribute setter.
+        Attempts first to change the attribute in self.ode, if applicable.
+        """
+
+        if "ode" in self.__dict__ and hasattr(self.ode, name):
+            setattr(self.ode, name, value)
+
+        else:
+            super().__setattr__(name, value)
