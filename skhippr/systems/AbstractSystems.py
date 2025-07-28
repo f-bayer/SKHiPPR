Using FD in AbstractCycle instead of FirstOrderODE makes HBM and shooting FRC match -- but both have wrong closed form derivatives

skhippr/systems/AbstractSystems.py
@@ -235,37 +235,37 @@ class FirstOrderODE(AbstractEquation):
             update = True
 
         ########## DEBUGGING always use finite differences
-        if True:  # variable in ("X", "omega"):
-            warnings.warn("Override closed form derivative in FirstOrderODE")
-            print("Override closed form derivative in FirstOrderODE")
-            if t is not None:
-                t_old = self.t
-                self.t = t
+        # if True:  # variable in ("X", "omega"):
+        #     warnings.warn("Override closed form derivative in FirstOrderODE")
+        #     print(f"Override closed form derivative w.rt. {variable} in FirstOrderODE")
+        #     if t is not None:
+        #         t_old = self.t
+        #         self.t = t
 
-            if x is not None:
-                x_old = self.x
-                self.x = x
+        #     if x is not None:
+        #         x_old = self.x
+        #         self.x = x
 
-            derivative = self.finite_difference_derivative(variable, h_step=h_fd)
+        #     derivative = self.finite_difference_derivative(variable, h_step=h_fd)
 
-            if t is not None:
-                self.t = t_old
+        #     if t is not None:
+        #         self.t = t_old
 
-            if x is not None:
-                self.x = x_old
+        #     if x is not None:
+        #         self.x = x_old
 
-            # Check sizes
-            cols_expected = np.atleast_1d(getattr(self, variable)).shape[0]
-            rows_expected = self.residual(update=False).shape[0]
-            others_expected = self.residual(update=False).shape[1:]
-            if derivative.shape != (rows_expected, cols_expected, *others_expected):
-                raise ValueError(
-                    f"Size mismatch in derivative w.r.t. '{variable}': Expected {(rows_expected, cols_expected, *others_expected)}, got {derivative.shape[:2]}"
-                )
+        #     # Check sizes
+        #     cols_expected = np.atleast_1d(getattr(self, variable)).shape[0]
+        #     rows_expected = self.residual(update=False).shape[0]
+        #     others_expected = self.residual(update=False).shape[1:]
+        #     if derivative.shape != (rows_expected, cols_expected, *others_expected):
+        #         raise ValueError(
+        #             f"Size mismatch in derivative w.r.t. '{variable}': Expected {(rows_expected, cols_expected, *others_expected)}, got {derivative.shape[:2]}"
+        #         )
 
-            self._derivative_dict[variable] = derivative
+        #     self._derivative_dict[variable] = derivative
 
-            return derivative
+        #     return derivative
         ##########
 
         if not update:
@@ -348,6 +348,30 @@ class AbstractCycleEquation(AbstractEquation):
 
         else:
             super().__setattr__(name, value)
+
+    @override
+    def derivative(self, variable, update=False, h_fd=0.0001):
+        # return super().derivative(variable, update, h_fd)
+        ######### DEBUGGING always use finite differences
+        if True:  # variable in ("X", "omega"):
+            warnings.warn("Override closed form derivative in AbstractCycle")
+            print(f"Override closed form derivative w.r.t. {variable} in AbstractCycle")
+
+            derivative = self.finite_difference_derivative(variable, h_step=h_fd)
+
+            # Check sizes
+            cols_expected = np.atleast_1d(getattr(self, variable)).shape[0]
+            rows_expected = self.residual(update=False).shape[0]
+            others_expected = self.residual(update=False).shape[1:]
+            if derivative.shape != (rows_expected, cols_expected, *others_expected):
+                raise ValueError(
+                    f"Size mismatch in derivative w.r.t. '{variable}': Expected {(rows_expected, cols_expected, *others_expected)}, got {derivative.shape[:2]}"
+                )
+
+            self._derivative_dict[variable] = derivative
+
+            return derivative
+        ##########
 
     def __copy__(self):
         # Shallow-copy everything manually without calling copy

test/test_shooting_frc.py
@@ -26,16 +26,18 @@ def shooting_frc(solver=None, visualize=True):
 
     omega_range = (0.1, 3)
 
-    # print("HBM reference")
-    # t_init = np.linspace(0, 2 * np.pi / ode.omega, 150, endpoint=False)
-    # x_init = initial_system.equations[0].x_time(t_init)
-    # frc_ref = frc_HBM(solver, ode, x_init=x_init)
-    # print("HBM reference done.")
-
     frc = []
     if visualize:
         fig = plt.figure()
         ax = fig.add_subplot(111, projection="3d")
+    else:
+        ax = None
+
+    print("HBM reference")
+    t_init = np.linspace(0, 2 * np.pi / ode.omega, 150, endpoint=False)
+    x_init = initial_system.equations[0].x_time(t_init)
+    frc_ref = frc_HBM(solver, ode, x_init=x_init, ax=ax)
+    print("HBM reference done.")
 
     # solver.verbose = False
     for branch_point in pseudo_arclength_continuator(
@@ -46,7 +48,7 @@ def shooting_frc(solver=None, visualize=True):
         initial_direction=1,
         continuation_parameter="T",
         verbose=True,
-        num_steps=1000,
+        num_steps=100,
     ):
         frc.append(branch_point)
         branch_point.determine_tangent()
@@ -58,6 +60,7 @@ def shooting_frc(solver=None, visualize=True):
             amp_2 = (branch_point.x[1], x_tng[1])
             ax.plot(Ts, amp_1, amp_2, "gray")
             ax.plot(Ts[0], amp_1[0], amp_2[0], ".", color="red")
+
             pass
 
         # ax.plot(
@@ -120,7 +123,7 @@ def shooting_frc(solver=None, visualize=True):
     pass
 
 
-def frc_HBM(solver, ode, x_init):
+def frc_HBM(solver, ode, x_init, ax=None):
     """Reference FRC determined using HBM"""
     N_HBM = 25
     L_DFT = x_init.shape[1]
@@ -135,14 +138,21 @@ def frc_HBM(solver, ode, x_init):
         pseudo_arclength_continuator(
             initial_system=HBM_init,
             solver=solver,
-            stepsize=0.4,
+            stepsize=0.2,
             stepsize_range=(0.05, 0.6),
             continuation_parameter="omega",
             verbose=True,
-            num_steps=80,
+            num_steps=50,
             initial_direction=-1,
         )
     )
+
+    if ax is not None:
+        for bp in frc:
+            x = bp.equations[0].x_time()[:, 0]
+            T = 2 * np.pi / bp.omega
+            ax.plot(T, x[0], x[1], ".", color="green")
+            pass
 
     return frc
 