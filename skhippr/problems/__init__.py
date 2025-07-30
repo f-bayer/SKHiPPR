10611581e02fcf99c1b0fb15e704b9ca4b3709a4
Fabia Bayer <bayer@inm.uni-stuttgart.de>
2025-07-30 16:59:13 +0200

    Keep the init file (for now)

--- skhippr/problems/__init__.py
+++
@@ -1,4 +0,0 @@
-from skhippr.solvers.newton import NewtonSolver
-from skhippr.cycles.hbm import HBMEquation
-from skhippr.cycles.shooting import ShootingBVP
-from skhippr.solvers.continuation import BranchPoint, pseudo_arclength_continuator
