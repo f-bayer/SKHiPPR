8e4481167b24df112cab9ad76de1db46943ef256
Fabia Bayer <bayer@inm.uni-stuttgart.de>
2025-07-30 18:29:45 +0200

    fix Duffing example

--- examples/duffing.py
+++ examples/duffing.py
@@ -8,8 +8,12 @@ import matplotlib.pyplot as plt
 # --- Fourier configuration ---
 from skhippr.Fourier import Fourier
 
+# --- Differential equation ---
+from skhippr.odes.nonautonomous import Duffing
+
 # --- HBM equation system ---
 from skhippr.cycles.hbm import HBMEquation
+from skhippr.equations.EquationSystem import EquationSystem
 
 # --- Stability method ---
 from skhippr.stability.KoopmanHillProjection import KoopmanHillSubharmonic
@@ -17,10 +21,13 @@ from skhippr.stability.KoopmanHillProjection import KoopmanHillSubharmonic
 # --- Continuation ---
 from skhippr.solvers.continuation import pseudo_arclength_continuator, BranchPoint
 
+# --- Newton solver ---
+from skhippr.solvers.newton import NewtonSolver
+
 
 def main():
     """
-    Demonstration for creating a simple frequency response curve with HBM.
+    Demonstration for creating a simple frequency response curve of a non-autonomous system with HBM.
 
     This function performs the following steps:
 
@@ -37,87 +44,87 @@ def main():
     None
     """
 
-    # instantiation of the ODE
-
-    # --- FFT, stability method configuration ---
+    # --- FFT, stability method and Newton solver configuration ---
     fourier = Fourier(N_HBM=25, L_DFT=300, n_dof=2, real_formulation=True)
     stability_method = KoopmanHillSubharmonic(
         fourier=fourier, tol=1e-4, autonomous=False
     )
+    solver = NewtonSolver(verbose=True)
 
-    # --- HBM problem setup: Define keyword args for system function ---
-    parameters = {"F": 0.5, "omega": 1}
-    omega = parameters["omega"]
-
-    # --- Define continuation parameter (here: could be 'omega' or 'F')
-    key_param = "omega"
+    # --- Instantiation of the ODE at initial point of branch ---
+    ode = Duffing(t=0, x=[1.0, 0.0], alpha=1, beta=2, delta=0.16, F=0.5, omega=1)
 
     # --- Initial guess in time and frequency domain ---
-    ts = fourier.time_samples(omega)
-    x0_samples = np.array([np.cos(omega * ts), -omega * np.sin(omega * ts)])
+    ts = fourier.time_samples(ode.omega)
+    x0_samples = np.array([np.cos(ode.omega * ts), -ode.omega * np.sin(ode.omega * ts)])
     X0 = fourier.DFT(x0_samples)
 
-    initial_problem = HBMEquation(
-        f=system_function,
-        omega=omega,
-        initial_guess=X0,
+    # --- Set up the Harmonic Balance equations
+    hbm = HBMEquation(
+        ode=ode,
+        omega=ode.omega,
         fourier=fourier,
-        stability_method=stability_method,
+        initial_guess=X0,
         period_k=1,
-        parameters_f=parameters,
-        verbose=True,
+        stability_method=stability_method,
     )
 
-    # --- Solve initial HBM problem---
-    initial_problem.solve()
-    print(initial_problem)
-    assert initial_problem.converged
+    # --- Solve initial HBM problem. ---
+    # As hbm is here an AbstractEquation and not an EquationSystem, we need to use solver.solve_equation() and specify the unknown variable explicitly.
+    # solver.solve_equation() internally instantiates an EquationSystem, just containing hbm.
+    solver.solve_equation(equation=hbm, unknown="X")
 
-    # --- Avoid that the HBM problem prints a full report at each continuation step
-    initial_problem.verbose = False
+    # --- Avoid that the solver prints a full report at each continuation step
+    solver.verbose = False
 
-    # --- Force response continuation ---
-    freqs = []
-    amps = []
-    branch = []
-    stable = []
-    unstable = []
+    # --- Creation of EquationSystem ---
+    # The solver needs an EquationSystem and not just an equation, so we have to create one.
+    # Alternatively to this explicit creation, one can also instantiate a HBMSystem.
+    initial_system = EquationSystem(
+        equations=[hbm], unknowns=["X"], equation_determining_stability=hbm
+    )
 
+    # --- Preallocate the result of the FRC continuation ---
+    # BranchPoints (a subclass of EquationSystem) extend the initial EquationSystem with one added equation for the tangency condition.
+    frc: list[BranchPoint] = []
+
+    # --- Iterate through the branch. Almost all arguments are optional. ---
     for branch_point in pseudo_arclength_continuator(
-        initial_problem=initial_problem,
+        initial_system=initial_system,
+        solver=solver,
         stepsize=0.1,
         stepsize_range=(0.001, 0.1),
-        key_param="omega",
-        value_param=omega,
+        continuation_parameter="omega",
+        initial_direction=1,
         verbose=True,
         num_steps=4000,
     ):
-        branch.append(branch_point)
-        stable.append(branch_point.stable)
-        unstable.append(not branch_point.stable)
-        freqs.append(branch_point.omega)
-
-        # Maximum amplitude of first DOF over a period
-        amps.append(np.max(branch_point.x_time()[1, :]))
+        frc.append(branch_point)
 
         # break if omega exceeds maximum
         if branch_point.omega > 2.5:
             break
 
+    # --- Analyze the branch. ---
+    freqs = np.array([np.squeeze(point.omega) for point in frc])
+    stable = np.array([point.stable for point in frc])
+
+    # --- Determine maximum x_1 amplitude of every point on the branch. ---
+    # the first equation of the BranchPoint equation system is the HBMequation, which provides x_time().
+    amps = np.array([np.max(point.equations[0].x_time()[0, :]) for point in frc])
+
     # --- plot ---
     plt.figure()
 
-    freqs_stable = np.array(freqs)
-    freqs_stable[unstable] = np.nan
+    freqs_stable = np.where(stable, freqs, np.nan)
+    freqs_unstable = np.where(~stable, freqs, np.nan)
 
-    freqs_unstable = np.array(freqs)
-    freqs_unstable[stable] = np.nan
+    plt.plot(freqs_stable, amps, "r", label="stable")
+    plt.plot(freqs_unstable, amps, "b--", label="unstable")
 
-    plt.plot(freqs_stable, amps, color="blue")
-    plt.plot(freqs_unstable, amps, color="red")
-
-    plt.xlabel(key_param)
-    plt.ylabel("|x_1|")
+    plt.xlabel("$\\omega$")
+    plt.ylabel("$|x_1|$")
+    plt.legend()
 
 
 if __name__ == "__main__":
